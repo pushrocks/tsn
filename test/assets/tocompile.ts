48bc518bc0f8378d8a0b6bca0614d3b4a1c7fef8
Phil Kunz <github@philkunz.com>
2016-06-29 20:18:51 +0200

    initial

---
+++ test/assets/tocompile.ts
@@ -0,0 +1,10 @@
+console.log("test");
+console.log("test2");
+
+class test {
+    test = []; 
+    constructor(){
+        console.log("hi")
+    };
+};
+
---
+++ ts/index.ts
@@ -0,0 +1,3 @@
+import * as plugins from "./tsn.plugins";
+
+plugins.shelljs.exec("cd test & tsc index.ts");
