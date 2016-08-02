e6d5d88e09dbbd8deffc15eeb4ec63d2442e4ed4
PhilKunz <github@philkunz.com>
2016-08-02 23:00:56 +0200

    update test

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,7 @@
+import "typings-test";
 import * as smartpath from "smartpath";
 import * as tsn from "../dist/index";
+import should = require("should");
 
 let assetfiles:string[] = [
     "./test/assets/tocompile.ts",
@@ -7,4 +9,4 @@ let assetfiles:string[] = [
 ];
 let assetfilesAbsolute:string[] = smartpath.transform.toAbsolute(assetfiles,process.cwd());
 console.log(assetfilesAbsolute);
-tsn.compile(assetfilesAbsolute,smartpath.transform.toAbsolute("./test/assets/output",process.cwd()));
+tsn.compile(assetfilesAbsolute,smartpath.transform.toAbsolute("./test/assets/output",process.cwd()),{});
