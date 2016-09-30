e9d4505403b70e1ec307a8153677d6bd08c4d36a
PhilKunz <github@philkunz.com>
2016-09-30 12:03:22 +0200

    clean up

--- test/assets/output/tocompile2.js
+++
@@ -1,2 +0,0 @@
-console.log("hello");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidG9jb21waWxlMi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3RvY29tcGlsZTIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxPQUFPLENBQUMsQ0FBQyJ9
