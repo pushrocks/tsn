10f456aa86abfdc18f91ec57be42970cdf4694bd
PhilKunz <github@philkunz.com>
2016-10-22 01:55:10 +0200

    update tsn

--- dist/tsn.plugins.js
+++ dist/tsn.plugins.js
@@ -3,8 +3,7 @@ require("typings-global");
 exports.beautylog = require("beautylog");
 exports.lodash = require("lodash");
 exports.q = require("q");
-exports.shelljs = require("shelljs");
 exports.smartpath = require("smartpath");
 exports.smartfile = require("smartfile");
 exports.typescript = require("typescript");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHNuLnBsdWdpbnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy90c24ucGx1Z2lucy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsMEJBQXVCO0FBQ3ZCLHlDQUE4QztBQUM5QyxtQ0FBd0M7QUFDeEMseUJBQThCO0FBQzlCLHFDQUEwQztBQUMxQyx5Q0FBOEM7QUFDOUMseUNBQThDO0FBQzlDLDJDQUFnRCJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHNuLnBsdWdpbnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy90c24ucGx1Z2lucy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsMEJBQXVCO0FBQ3ZCLHlDQUE4QztBQUM5QyxtQ0FBd0M7QUFDeEMseUJBQThCO0FBQzlCLHlDQUE4QztBQUM5Qyx5Q0FBOEM7QUFDOUMsMkNBQWdEIn0=
--- ts/tsn.plugins.ts
+++ ts/tsn.plugins.ts
@@ -2,7 +2,6 @@ import 'typings-global'
 export import beautylog = require('beautylog')
 export import lodash = require('lodash')
 export import q = require('q')
-export import shelljs = require('shelljs')
 export import smartpath = require('smartpath')
 export import smartfile = require('smartfile')
 export import typescript = require('typescript')
